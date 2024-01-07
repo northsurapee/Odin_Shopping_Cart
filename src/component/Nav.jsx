79d35ff18e77d9ca59fa212b1580be62c5a78f83
Surapee <suwan.srp@gmail.com>
2024-01-07 17:55:40 +0700

    Update nav bar to have logo and cart button with basic styles

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,11 +1,19 @@
 import "../styles/Nav.css"
+import logoImage from "../assets/logo.svg"
+import cartIcon from "../assets/cart.svg"
 import { Link } from "react-router-dom";
 
 export default function Nav() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/shop">Shop</Link>
-        </nav>
+            <nav>
+                <div className="logo-container">
+                    <img className="logo" src={logoImage} alt="logo" />
+                </div>
+                <div className="link-container">
+                    <Link to="/">Home</Link>
+                    <Link to="/shop">Shop</Link>
+                    <img className="cart" src={cartIcon} alt="cart" />
+                </div>
+            </nav>
     )
 }
