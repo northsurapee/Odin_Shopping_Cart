3d463bcf3facc3b86abf173308baa1d6f5a535f3
Surapee <suwan.srp@gmail.com>
2024-01-08 12:31:11 +0700

    Add section two of home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import textBackground from "../assets/innovate-text-gradient.png";
 import heroImg from "../assets/hero-img.png";
 import lipimg from "../assets/lips.png";
 import arrow from "../assets/arrow.svg";
-// import smilingFace from "../assets/bestbuy-face.png";
+import smilingFace from "../assets/bestbuy-face.png"
 import { Link } from "react-router-dom";
 import "../styles/Home.css"
 import Marquee from "../component/Marquee";
@@ -49,6 +49,18 @@ export default function Home() {
                 </div>
             </section>
             <Marquee content={"THE JOURNEY STARTS HERE"} />
+            <section id="two">
+                <div className="left">
+                    <img src={smilingFace} alt="smiling-face" />
+                    <Link to="/shop">MEET THE NEW AGE</Link>
+                </div>
+                <div className="right">
+                    <h2>Tech is the <b>new age.</b></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elementum, nisl nec dictum vehicula, sapien metus convallis nisi, in tristique velit nulla tempor tortor. Ut vitae orci id purus efficitur viverra quis ut purus. Ut posuere vel augue a aliquet. Integer at erat lobortis, bibendum ex vitae, volutpat eros. Suspendisse euismod placerat nisi quis maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris volutpat pharetra malesuada. Cras non nisl imperdiet, posuere nunc quis, lobortis dui. Fusce mollis bibendum libero in ultrices. Nulla vulputate ullamcorper diam eget eleifend. Nulla id vehicula quam. Suspendisse dui justo, aliquam finibus lacinia eu, porta eget turpis.</p>
+                    <br />
+                    <p>Quisque condimentum tempor cursus. Nulla nunc massa, porta a venenatis in, ornare at arcu. Nam pharetra, dolor id suscipit semper, nisl tellus lacinia ligula, et sollicitudin nibh lorem eget metus. Donec in aliquam tellus.</p>
+                </div>
+            </section>
             <Footer />
         </>
     )
