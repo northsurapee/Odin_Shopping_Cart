1c8b4bd54e7cbd7bdd80b77bedba4c8155cb42b4
Surapee <suwan.srp@gmail.com>
2025-03-17 00:07:31 +0700

    fix: update api endpoint

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,6 +1,6 @@
-import { useFetch } from "../../hooks/useFetch"
+import { useFetch } from "../../hooks/useFetch";
 import { useParams, useSearchParams } from "react-router-dom";
-import "../../styles/Shop.css"
+import "../../styles/Shop.css";
 import { ItemsList } from "./ ItemList";
 import { Pagination } from "../../component/Pagination";
 import { useEffect } from "react";
@@ -9,71 +9,76 @@ import { categoriesData } from "./categoriesData";
 import { SortItems } from "./SortItems";
 
 export default function Shop() {
-    console.log("Render-Shop");
-    const { category } = useParams();
-    const [searchParams] = useSearchParams();
-    const currCategoryId = category || "abcat0712000";
-    const currPage = searchParams?.get("page") || 1;
-    const sortBy = searchParams?.get("sort") || "customerReviewCount.dsc";
+  console.log("Render-Shop");
+  const { category } = useParams();
+  const [searchParams] = useSearchParams();
+  const currCategoryId = category || "abcat0712000";
+  const currPage = searchParams?.get("page") || 1;
+  const sortBy = searchParams?.get("sort") || "customerReviewCount.dsc";
 
-    const categories = categoriesData.subCategories;
+  const categories = categoriesData.subCategories;
 
-    // apiKey=wPuxAJ5Tl0BBktS2G1ihzmAT
-    const [loadingItems, errorItems, itemsData, anticipateFetch] = useFetch(`https://api.bestbuy.com/v1/products(categoryPath.id=${currCategoryId})?apiKey=VIHFIWLgsIL8eIpAu8Sc1UlM&sort=${sortBy}&show=categoryPath.id,categoryPath.name,customerReviewAverage,customerReviewCount,image,name,onSale,percentSavings,regularPrice,salePrice,shortDescription,sku&pageSize=18&page=${currPage}&format=json`);
+  // apiKey=wPuxAJ5Tl0BBktS2G1ihzmAT
+  const [loadingItems, errorItems, itemsData, anticipateFetch] = useFetch(
+    `https://api.bestbuy.com/v1/products(categoryPath.id=${currCategoryId})?apiKey=uGV5YibQ4AjP08YuJ5hL5ir1&sort=${sortBy}&show=categoryPath.id,categoryPath.name,customerReviewAverage,customerReviewCount,image,name,onSale,percentSavings,regularPrice,salePrice,shortDescription,sku&pageSize=18&page=${currPage}&format=json`,
+  );
 
-    useEffect(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-      }, [category, currPage]);
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, [category, currPage]);
 
-    if (errorItems)
+  if (errorItems)
     return (
-        <>
-                <div className="error">
-                    <h1>An error occurred while loading the data!</h1>
-                    <span>Please, try again...</span>
-                </div>
-        </>
+      <>
+        <div className="error">
+          <h1>An error occurred while loading the data!</h1>
+          <span>Please, try again...</span>
+        </div>
+      </>
     );
 
-    return (
+  return (
+    <>
+      {!itemsData ? (
+        <div className="loader-container">
+          <div className="loader"></div>
+        </div>
+      ) : (
         <>
-            {!itemsData ? (
-                <div className="loader-container">
-                    <div className="loader"></div>
-                </div>
-            ) : (
-                <>
-                <div className="shop-background">
-                    <h1>GAMING STORE</h1>
-                </div>
-                <div className="shop-main">
-                    <div className="left-side-bar">
-                        <Sidebar
-                            currCategory={category}
-                            categories={categories}
-                            onCategoryChange={anticipateFetch}
-                        />
-                    </div>
-                    <div className="middle-result" style={{ opacity: loadingItems ? 0.5 : 1 }}>
-                        <div className="top-result">
-                            <span className="total-item">{itemsData.total} items</span>
-                            <SortItems onChange={anticipateFetch} />
-                        </div>
-                        <ItemsList items={itemsData.products}/>
-                        <div>
-                        {+itemsData.totalPages && (
-                            <Pagination
-                            currPage={+currPage}
-                            totalPages={+itemsData.totalPages}
-                            siblings={3}
-                            onPageChange={anticipateFetch}
-                            />
-                        )}
-                        </div>
-                    </div>
-                </div>
-                </>
-            )}
+          <div className="shop-background">
+            <h1>GAMING STORE</h1>
+          </div>
+          <div className="shop-main">
+            <div className="left-side-bar">
+              <Sidebar
+                currCategory={category}
+                categories={categories}
+                onCategoryChange={anticipateFetch}
+              />
+            </div>
+            <div
+              className="middle-result"
+              style={{ opacity: loadingItems ? 0.5 : 1 }}
+            >
+              <div className="top-result">
+                <span className="total-item">{itemsData.total} items</span>
+                <SortItems onChange={anticipateFetch} />
+              </div>
+              <ItemsList items={itemsData.products} />
+              <div>
+                {+itemsData.totalPages && (
+                  <Pagination
+                    currPage={+currPage}
+                    totalPages={+itemsData.totalPages}
+                    siblings={3}
+                    onPageChange={anticipateFetch}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
         </>
-    );
+      )}
+    </>
+  );
 }
--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import "../../styles/SingleProduct.css"
+import "../../styles/SingleProduct.css";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import Price from "../../component/Price";
@@ -8,86 +8,90 @@ import { AdditionalInfo } from "./AdditionalInfo";
 import Rating from "../../component/Rating";
 import { decode } from "html-entities";
 
-export default function SingleProduct({onAdd}) {
-    console.log("Render-SingleProduct")
-    const { id } = useParams();
-    const [loading, error, rawData] = useFetch(`https://api.bestbuy.com/v1/products(sku=${id})?apiKey=VIHFIWLgsIL8eIpAu8Sc1UlM&show=addToCartUrl,categoryPath.id,categoryPath.name,color,customerReviewAverage,customerReviewCount,description,details.name,details.value,features.feature,image,images,includedItemList.includedItem,longDescription,manufacturer,modelNumber,name,onSale,regularPrice,salePrice,shortDescription,sku,productVariations&format=json`);
-    if (loading || error) return (
-        <>
-            <div className="loader-container">
-                <div className="loader"></div>
-            </div>
-        </>
-    );
-    const data = rawData.products[0];
-
+export default function SingleProduct({ onAdd }) {
+  console.log("Render-SingleProduct");
+  const { id } = useParams();
+  const [loading, error, rawData] = useFetch(
+    `https://api.bestbuy.com/v1/products(sku=${id})?apiKey=uGV5YibQ4AjP08YuJ5hL5ir1&show=addToCartUrl,categoryPath.id,categoryPath.name,color,customerReviewAverage,customerReviewCount,description,details.name,details.value,features.feature,image,images,includedItemList.includedItem,longDescription,manufacturer,modelNumber,name,onSale,regularPrice,salePrice,shortDescription,sku,productVariations&format=json`,
+  );
+  if (loading || error)
     return (
-        <>
-            <div className="main-product">
-                <div className="carousel-container">
-                    <Carousel images={data.images} />
-                </div>
-                <div className="info-container">
-                    <div className="model-sku-container">
-                        <div className="model-container">
-                            <b>Model: </b>
-                            {data.modelNumber}
-                        </div>
-                        <div className="sku-container">
-                            <b>SKU: </b>
-                            {data.sku}
-                        </div>
-                    </div>
-                    <h1 className="product-name">{data.name}</h1>
-                    <Rating
-                        itemRating={data.customerReviewAverage}
-                        reviewCount={data.customerReviewCount}
-                        showRating={true}
-                    />
-                    <Price
-                    regularPrice={data.regularPrice}
-                    salePrice={data.salePrice}
-                    onSale={data.onSale}
-                    />
-                    {data.color && (
-                        <div className="color-container">
-                            <b>Color: </b>
-                            {data.color}
-                        </div>
-                    )}
-                    <hr className="underline"></hr>
-                    <div className="description-container">
-                        <p>{decode(data.longDescription)}</p>
-                        <br />
-                        <p>
-                            Proin varius magna nec laoreet viverra. Maecenas eleifend, diam et vulputate
-                            facilisis, nunc purus fermentum neque, vitae vestibulum lectus nunc a
-                            turpis. Curabitur tincidunt arcu et ex posuere, id imperdiet purus iaculis.
-                            Fusce efficitur tincidunt purus vitae sagittis. Nunc commodo nibh eget
-                            condimentum iaculis. Ut erat massa, gravida eget urna sit amet, tristique
-                            posuere erat. Aliquam eu mollis massa. Proin venenatis lectus vitae
-                            scelerisque dapibus. Duis auctor lectus id nisi viverra congue. Curabitur et
-                            semper purus. Cras vel lacus sit amet nulla rutrum venenatis et vel nunc.
-                            Curabitur dignissim auctor mollis. Integer mattis vehicula elit ac
-                            scelerisque. Nullam tempor massa erat, et feugiat massa luctus a. Nullam
-                            viverra malesuada purus at fermentum. Pellentesque id imperdiet ex.
-                        </p>
-                    </div>
-                    <button 
-                        type="button" 
-                        className="add-to-bag-button"
-                        onClick={() => onAdd(data)}
-                    >
-                        ADD TO BAG
-                    </button>
-                </div>
-            </div>
-            <AdditionalInfo
-                    features={data.features}
-                    included={data.includedItemList}
-                    details={data.details}
-            />
-        </>
+      <>
+        <div className="loader-container">
+          <div className="loader"></div>
+        </div>
+      </>
     );
-}
+  const data = rawData.products[0];
 
+  return (
+    <>
+      <div className="main-product">
+        <div className="carousel-container">
+          <Carousel images={data.images} />
+        </div>
+        <div className="info-container">
+          <div className="model-sku-container">
+            <div className="model-container">
+              <b>Model: </b>
+              {data.modelNumber}
+            </div>
+            <div className="sku-container">
+              <b>SKU: </b>
+              {data.sku}
+            </div>
+          </div>
+          <h1 className="product-name">{data.name}</h1>
+          <Rating
+            itemRating={data.customerReviewAverage}
+            reviewCount={data.customerReviewCount}
+            showRating={true}
+          />
+          <Price
+            regularPrice={data.regularPrice}
+            salePrice={data.salePrice}
+            onSale={data.onSale}
+          />
+          {data.color && (
+            <div className="color-container">
+              <b>Color: </b>
+              {data.color}
+            </div>
+          )}
+          <hr className="underline"></hr>
+          <div className="description-container">
+            <p>{decode(data.longDescription)}</p>
+            <br />
+            <p>
+              Proin varius magna nec laoreet viverra. Maecenas eleifend, diam et
+              vulputate facilisis, nunc purus fermentum neque, vitae vestibulum
+              lectus nunc a turpis. Curabitur tincidunt arcu et ex posuere, id
+              imperdiet purus iaculis. Fusce efficitur tincidunt purus vitae
+              sagittis. Nunc commodo nibh eget condimentum iaculis. Ut erat
+              massa, gravida eget urna sit amet, tristique posuere erat. Aliquam
+              eu mollis massa. Proin venenatis lectus vitae scelerisque dapibus.
+              Duis auctor lectus id nisi viverra congue. Curabitur et semper
+              purus. Cras vel lacus sit amet nulla rutrum venenatis et vel nunc.
+              Curabitur dignissim auctor mollis. Integer mattis vehicula elit ac
+              scelerisque. Nullam tempor massa erat, et feugiat massa luctus a.
+              Nullam viverra malesuada purus at fermentum. Pellentesque id
+              imperdiet ex.
+            </p>
+          </div>
+          <button
+            type="button"
+            className="add-to-bag-button"
+            onClick={() => onAdd(data)}
+          >
+            ADD TO BAG
+          </button>
+        </div>
+      </div>
+      <AdditionalInfo
+        features={data.features}
+        included={data.includedItemList}
+        details={data.details}
+      />
+    </>
+  );
+}
